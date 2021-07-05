82c67221c552970871370f54b3e0b4d81540b6b4
Angel <kayron013@gmail.com>
2021-07-04 21:47:31 -0700

    Add commands to README

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # <img alt="watchlist icon" src="res/icon/icon.jpg" height="40"/> Watchlist
 
 A discord bot for maintaining a list of movies and shows.
+
+## Commands
+
+`/item add` - Add an item to a watchlist
+
+`/item delete` - Remove an item from a watchlist
+
+`/item reorder` - Reorder an item in a watchlist
+
+`/list add` - Add a watchlist
+
+`/list all` - View all watchlists
+
+`/list delete` - Remove a watchlist
+
+`/list items` - View the entries in a watchlist
+
+`/lists` - Alias for `/list all`
--- functions/src/events/updateListLength.ts
+++ functions/src/events/updateListLength.ts
@@ -7,16 +7,19 @@ export const updateListLength = f.document('owners/{owner}/lists/{list}/items/{i
 
   const lengthChange = change.before.exists ? -1 : 1;
 
-  const listRef = change.after.ref.parent.parent;
-
-  if (!listRef) return;
+  const listRef = change.after.ref.parent.parent!;
 
   await listRef
     .update({
       length: firestore.FieldValue.increment(lengthChange),
     })
     .catch(e => {
+      // The list does not exist
+      if (e.code === 5) {
+        return;
+      }
       logger.error('Error updating list length', {
+        errorCode: e.code,
         item: change.after.ref.path,
         list: listRef.path,
         beforeExists: change.before.exists,
